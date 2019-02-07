ad9cb995849205ea24abbbc1f8c61c9f65c59df8
Garret Wasden <gkwazy@gmail.com>
2019-02-07 15:41:51 -0700

    updated app

--- src/App.js
+++ src/App.js
@@ -1,18 +1,17 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import Home from './pages/homepage'
-import Navbar from './pages/Navbar'
-
+import Home from "./pages/homepage";
+import Navbar from "./components/Navbar";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Navbar />
-        <Home />
-      </div>
+      <Router>
+        <div className="App">
+          <h1>Wolf game man</h1>
+        </div>
+      </Router>
     );
   }
 }
