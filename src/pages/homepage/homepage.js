4d7c89fb5197d3bc84ffae8094d13036a661579b
Garret Wasden <gkwazy@gmail.com>
2019-02-20 14:59:42 -0700

    backbone

--- src/pages/homepage/homepage.js
+++ src/pages/homepage/homepage.js
@@ -1,9 +1,16 @@
-import React from 'react';
+import React, { Component } from "react";
 
-
-const MyComponent = () => (
-    <div>
-        <h1> welcome to the page </h1>
-    </div>
-);
-export default MyComponent;
+class HomePage extends Component {
+  render() {
+    return (
+      <div className="homePage container">
+        <div className="row">
+          <div className="col-md-12">
+            <h1>Hello world</h1>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+export default HomePage;
